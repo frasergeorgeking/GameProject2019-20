9ff70f5a9a2b5303cf9226ccc725244024969434
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2019-10-21 12:41:31 +0100

    Pushed Piano C3 Sample Note, Created TestScript, Updated References

---
+++ Unity Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+     // Start is called before the first frame update
+    void Start()
+    {
+        float[] noteLookup = new float[4];
+
+        noteLookup[0] = 1f;
+        noteLookup[1] = 2f;
+        noteLookup[2] = 3f;
+        noteLookup[3] = 4f;
+
+        foreach (float item in noteLookup)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
