071873f6df34172eff4b13a0e0cb72f9dfa7de02
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2019-10-30 16:35:03 +0000

    Created new AudioManager Script, Created AudioGameObject Prefab
    AudioGameObject currently spawn from Object Pooler, however Audio Manager script needs to be extended to recycle objects back into pool (currently not removed after a given note has been played)

---
+++ Unity Project/Assets/Scripts/AudioController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] AudioClip[] audioSamples;
+
+    private GameObject audioGameObject;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayNote(1f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayNote(1.059463f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayNote(1.122462f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PlayNote(1.189207f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            PlayNote(1.259921f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            PlayNote(1.334840f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            PlayNote(1.414214f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            PlayNote(1.498307f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            PlayNote(1.587401f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            PlayNote(1.681793f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            PlayNote(1.781797f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            PlayNote(1.887749f);
+        }
+    }
+
+    void PlayNote(float pitch)
+    {
+        //Pull reference to audioGameObject from pooler
+        audioGameObject = ObjectPooler.sharedInstance.GetPooledObject("audioGameObject");
+
+        if (audioGameObject != null)
+        {
+            audioGameObject.transform.position = new Vector3(0f, 0f, 0f);
+            audioGameObject.SetActive(true);
+        }
+
+        AudioSource audioSource = audioGameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioSamples[0]; //Debug Assign Clip
+        audioSource.pitch = pitch;
+        audioSource.PlayOneShot(audioSource.clip, 1f);
+
+    }
+
+}
