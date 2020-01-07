9bd183e07babdd8b7fe4162adebf67cae656f911
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2020-01-07 18:48:02 +0000

    Pushed minor changes to player controller

--- ProjectDex/Assets/Scripts/PlayerCharacter/PlayerController.cs
+++ ProjectDex/Assets/Scripts/PlayerCharacter/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //Editor-Facing Private Variables
     [SerializeField] float moveThrust = 100f;
+    [SerializeField] float deadZone = 0.25f;
 
     //Private Variables
     private Rigidbody2D rb;
@@ -41,10 +42,36 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        Debug.Log(move); //Debug Test
+        
     }
 
-    void Fire()
+    void FixedUpdate()
+    {
+        UpdatePlayerMovement();
+    }
+
+    private void UpdatePlayerMovement()
+    {
+        //Perform Dead-Zone Check on Stick Input
+        if ((move.magnitude < deadZone) || (-move.magnitude > deadZone))
+        {
+            move = Vector2.zero; //Zero-Out Movement if in Dead-Zone
+        }
+
+        //Calculate Stick Angle
+        float angle = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg;
+
+        //Perform Zero-Check on Angle; Prevents Player Character from Locking Upwards when Stick is Released/Dead-Zone Reached
+        if (angle != 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -angle)); //Update Rotation in Accordance with Angle
+        }
+
+
+    }
+
+
+    private void Fire()
     {
         Debug.Log("Fired"); //Debug Test
     }
