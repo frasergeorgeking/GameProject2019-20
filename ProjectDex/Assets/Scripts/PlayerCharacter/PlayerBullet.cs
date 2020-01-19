d92bd88bcd930630b31456d22537804b8c4f8f2a
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2020-01-19 19:49:19 +0000

    Implemented Bullet Firing System

---
+++ ProjectDex/Assets/Scripts/PlayerCharacter/PlayerBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    //Editor-Facing Private Variables
+    [SerializeField] [Range(0f, 10f)] float bulletSpeed;
+    
+    //Private Variables
+    private Rigidbody2D bulletRB;
+
+    void Awake()
+    {
+        bulletRB = GetComponent<Rigidbody2D>();
+    }
+
+    public float GetBulletSpeed()
+    {
+        return bulletSpeed;
+    }
+
+    public void SetBulletSpeed(float newBulletSpeed)
+    {
+        bulletSpeed = newBulletSpeed;
+    }
+
+}
--- ProjectDex/Assets/Scripts/PlayerCharacter/PlayerController.cs
+++ ProjectDex/Assets/Scripts/PlayerCharacter/PlayerController.cs
@@ -6,13 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     //Editor-Facing Private Variables
     [SerializeField] [Range(2f, 25f)] float topSpeedModifier = 2f;
-    [SerializeField] [Range(0.0f, 1f)] float deadZone = 0.365f;
+    [SerializeField] [Range(0f, 4f)] float shootCooldown = 0.2f;
+    [SerializeField] [Range(0f, 1f)] float leftStickDeadZone = 0.365f;
+    [SerializeField] [Range(0f, 1f)] float rightStickDeadZone = 0.365f;
 
     //Private Variables
     private Rigidbody2D playerRB;
     private PlayerControls controls; //PlayerControls class generated through new Input Manager, selected option on PlayerControls.inputactions - prevents manual string lookups on individual actions (e.g. "Fire")
     private Vector2 move; //'move' Vector2 used to map Axis data from left joystick to
     private Vector2 shoot; //'shoot' Vector2 used to map Axis data from right joystick to
+
+    private bool canShoot = true; //Used for shoot cooldown timer
+    private bool canBeHit = true; //Used to track invincibility frames - REQUIRES IMPLEMENTATION
+    
+    private GameObject bullet; //Container for bullet GameOject Reference
 
     void Awake()
     {
@@ -49,27 +56,39 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        UpdatePlayerMovement();
+        HandlePlayerInput();
     }
 
-    private void UpdatePlayerMovement()
-    {     
-        //Perform Dead-Zone Check on Stick Input
-        if ((move.magnitude < deadZone) || (-move.magnitude > deadZone))
+    private void HandlePlayerInput()
+    {
+        //Perform Dead-Zone Check on Left Stick Input
+        float absLeftStickMagnitude = Mathf.Abs(move.magnitude); //Absoloute Value used for Dead-Zone Comparison
+        if (absLeftStickMagnitude < leftStickDeadZone)
         {
-            move = Vector2.zero; //Zero-Out Movement if in Dead-Zone
+            move = Vector2.zero; //If in dead-zone, zero-out move Vector2
         }
 
         //Calculate Rotation Angle
-        float angle = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg;
+        float leftStickAngle = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg;
 
         //Perform Zero-Check on Angle; Prevents Player Character from Locking Upwards when Stick is Released/Dead-Zone Reached
-        if (angle != 0)
+        if (leftStickAngle != 0)
         {
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -angle)); //Update Rotation of Dex in Accordance with Angle
-            float playerAcceleration = CalculateSpeed(move.x, move.y); //Pass Stick Data to Thrust Algorithm
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -leftStickAngle)); //Update Rotation of Dex in Accordance with Angle
+            float playerSpeed = CalculateSpeed(move.x, move.y); //Pass Stick Data to Thrust Algorithm
+            playerRB.velocity = new Vector2(move.x * playerSpeed, move.y * playerSpeed); //Update rb velocity in accordance with stick data
+        }
 
-            playerRB.velocity = new Vector2(move.x * playerAcceleration, move.y * playerAcceleration);
+        //Perform Dead-Zone Check on Right Stick Input
+        float absRightStickMagnitude = Mathf.Abs(shoot.magnitude); //Absoloute Value used for Dead-Zone Comparison
+        if(absRightStickMagnitude < rightStickDeadZone)
+        {
+            shoot = Vector2.zero; //If in dead-zone, zero-out shoot Vector2
+        }
+
+        if ((absRightStickMagnitude > rightStickDeadZone) && (canShoot))
+        {
+            StartCoroutine(Shoot(shoot)); //If out of dead-zone AND canShoot, run Shoot Coroutine
         }
 
 
@@ -77,21 +96,53 @@ public class PlayerController : MonoBehaviour
 
     private float CalculateSpeed(float xPos, float yPos)
     {
-        float currentStickRadius = CalculateRadius(xPos, yPos); //Calculate stick radius to judge speed
-        float currentSpeedModifier = currentStickRadius * topSpeedModifier;
+        float currentStickRadius = CalculateRadius(xPos, yPos); //Calculate stick radius to judge speed - larger radius = higher speed
+        float currentSpeedModifier = currentStickRadius * topSpeedModifier; //Apply exposed modifier to stick radius to create currentSpeedModifier
 
         //Logarithmic Function to Taper Speed Value
         float currentSpeed = Mathf.Log(currentSpeedModifier, 1.16f);
                 
-        return currentSpeed; 
+        return currentSpeed; //Return speed float
     }
 
     private float CalculateRadius(float x, float y)
     {
         //Standard Form Equation of a Circle - a and b values not required as they are both a constant of zero - i.e. centre of analog stick always 0,0
         float radiusSquared = (x * x) + (y * y); // (x^2) + (y^2) = r^2
+        
         return Mathf.Sqrt(radiusSquared); //Return square root of radiusSquared
     }
 
+    private void FireBullet(Vector2 shootRef)
+    {
+        //Pull bullet Reference from Pooler
+        bullet = ObjectPooler.sharedInstance.GetPooledObject("bullet");
+
+        if (bullet != null) //Peform Null-Check on bullet
+        {
+            //Set bullet position & rotation to that of player character
+            bullet.transform.position = gameObject.transform.position;
+            bullet.transform.rotation = gameObject.transform.rotation;
+            bullet.SetActive(true); //Spawn Bullet
+
+            //Pull Reference to Neccesary bullet Components
+            Rigidbody2D bulletSpawnedRB = bullet.GetComponent<Rigidbody2D>();
+            PlayerBullet bulletPlayerBullet = bullet.GetComponent<PlayerBullet>();
+
+            //Set bullet Velocity
+            bulletSpawnedRB.velocity = new Vector2((shootRef.x * bulletPlayerBullet.GetBulletSpeed()), (shootRef.y * bulletPlayerBullet.GetBulletSpeed()));
+        }
+    }
+
+    IEnumerator Shoot(Vector2 shootRef)
+    {
+        //Run Firing Logic
+        FireBullet(shootRef);
+        canShoot = false;
+
+        //Cooldown
+        yield return new WaitForSeconds(shootCooldown);
+        canShoot = true;
+    }
  
 }
