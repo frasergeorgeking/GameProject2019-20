01326659e174bea4c2137c69ba593afa80b78faf
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2020-01-21 18:22:30 +0000

    Merged Latest Build with SJM 3D Background Fork
    Implemented Rotating 3D Background to Scene - using placeholder 3D models
    
    Co-Authored-By: samjmcmillan <samjmcmillan@users.noreply.github.com>

---
+++ ProjectDex/Assets/Scripts/PlanetRotator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotator : MonoBehaviour
+{
+    //Editor-Facing Private Variables
+    [SerializeField] [Range(1f, 50f)] float speed;
+    
+    //Private Variables
+    
+
+    public enum rotationAxis
+    {
+        xAxis,
+        yAxis,
+        zAxis
+    }
+
+    public rotationAxis desiredAxis;
+
+    private void FixedUpdate()
+    {
+        switch (desiredAxis)
+        {
+            case (rotationAxis.xAxis):
+                transform.Rotate(Vector3.left * (Time.deltaTime * speed));
+                break;
+
+            case (rotationAxis.yAxis):
+                transform.Rotate(Vector3.up * (Time.deltaTime * speed));
+                break;
+
+            case (rotationAxis.zAxis):
+                transform.Rotate(Vector3.forward * (Time.deltaTime * speed));
+                break;
+        }
+
+    }
+
+
+}
+
