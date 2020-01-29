8812f10ad057812735687e85cd82f9c86b02ef52
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2020-01-29 11:28:01 +0000

    Created ReferenceManager.cs, Made minor scene adjustments

---
+++ ProjectDex/Assets/Scripts/SingletonReferenceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonReferenceManager : MonoBehaviour
+{
+    //Create Shared Instance
+    private static SingletonReferenceManager sharedInstance;
+    public static SingletonReferenceManager Instance { get { return sharedInstance; } } //Getter, returns private sharedInstance
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        Debug.Log("player");
+    }
+
+
+    public GameObject GetGameObjectReference(string tagName)
+    {
+        return GameObject.FindGameObjectWithTag(tagName);
+    }
+}
