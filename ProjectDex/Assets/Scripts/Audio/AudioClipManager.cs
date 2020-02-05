0dab6ce457202049a4130a5ddf2a38ddb9b544b3
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2020-02-05 20:17:50 +0000

    Created Class to Hold Updated Bars, Populated Audio Manager in Scene with References
    Co-Authored-By: kt-millar <kt-millar@users.noreply.github.com>

--- ProjectDex/Assets/Scripts/Audio/AudioClipManager.cs
+++ ProjectDex/Assets/Scripts/Audio/AudioClipManager.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class AudioClipManager : MonoBehaviour
 {
     //Editor-Facing Private Variables
-    [SerializeField] AudioClip[] audioTracks;
+    [SerializeField] AudioClipData[] audioClipData;
 
-    //Getter Functions
+    [System.Serializable] //Allows editing in editor
+    public class AudioClipData //Creates class that contains neccessary data types
+    {
+        public AudioClip[] audioClips; //Audio clip(s) that comprise of the full track
+        public string trackName;
+    }
+
+    //Getter Functions 
+    /*
     public AudioClip GetAudioTrack(int trackNum)
     {
         return audioTracks[trackNum - 1]; //Return Requested Track (-1 because array counts from 0)
     }
+    */
+    
 }
