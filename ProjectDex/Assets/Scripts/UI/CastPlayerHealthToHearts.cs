94070164f8a8ea5d5ad77dafb73b727291f595f4
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2020-03-23 13:09:50 +0000

    Made further file stucture adjustments to Unity Project

---
+++ ProjectDex/Assets/Scripts/UI/CastPlayerHealthToHearts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CastPlayerHealthToHearts : MonoBehaviour
+{
+    //Editor-Facing Private Variables
+    [SerializeField] List<GameObject> heartGameObjects;
+    [SerializeField] GameObject player;
+
+    //Private Variables
+    private List<Image> heartImageComponents;
+    private PlayerController playerController;
+    
+    void Awake()
+    {
+        //Iterate Through heartGameObjects List & Add Image Component Reference to heartImageComponents List
+        foreach (GameObject heart in heartGameObjects)
+        {
+            heartImageComponents.Add(heart.GetComponent<Image>());
+        }
+
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+}
